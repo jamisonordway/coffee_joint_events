e41eb6c80e9104973408ad655074ca50d9cf3bb9
JamisonOrdway <jamisonordway@gmail.com>
2018-11-26 10:59:22 -0700

    Adds new token for only cannacoding events

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
   }
   
   getEventsList = async () => {
-    const response = await fetch('https://www.eventbriteapi.com/v3/events/search/?q=the+coffee+joint+Yuma&token=GV4WN6VQGEDOJ565HBV4')
+    const response = await fetch('https://www.eventbriteapi.com/v3/users/me/owned_events/?token=GN6HLNG4ADTY75G6JOGS')
+    console.log(response)
     const result = await response.json();
     const events = result.events
     this.setState({ eventsData: events });
